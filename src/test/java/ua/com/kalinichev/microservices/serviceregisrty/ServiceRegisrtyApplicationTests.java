801def2e090ebc556b437686351b6c3416ec032e
Hlib Kalinichev <hlib.kalinichev@ukma.edu.ua>
2022-10-10 09:48:18 +0300

    first commit

---
+++ src/test/java/ua/com/kalinichev/microservices/serviceregisrty/ServiceRegisrtyApplicationTests.java
@@ -0,0 +1,13 @@
+package ua.com.kalinichev.microservices.serviceregisrty;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceRegisrtyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
